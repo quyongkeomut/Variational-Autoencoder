c84085b9dc4044e310826cb68d08bc44896089a5
Phuoc-Thinh, N <thinh.nguyen.dlai@gmail.com>
2025-06-05 23:22:01 +0700

    add 2 options of training: DDP and single GPU

--- neural_nets/autoencoders/trainer.py
+++ neural_nets/autoencoders/trainer.py
@@ -12,7 +12,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim.swa_utils import AveragedModel, get_ema_multi_avg_fn
 from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR
 
-
+AVG_WEIGHT = 0.999
 today = datetime.today()
 formatted_today = today.strftime('%Y_%m_%d_%H_%M')
 
@@ -20,9 +20,11 @@ formatted_today = today.strftime('%Y_%m_%d_%H_%M')
 class AETrainer:
     def __init__(
         self,
+        is_ddp: bool,
         encoder: Type[Module],
         decoder: Type[Module],
         task: str,
+        base_lr: float,
         criterion,
         optimizer,
         num_epochs: int,
@@ -48,51 +50,27 @@ class AETrainer:
             out_dir (str, optional): _description_. Defaults to "./weights/AEweights".
             lr_scheduler_cosine (Optional[CosineAnnealingLR], optional): _description_. Defaults to None.
         """
+
+        self.task = task
+        self.is_ddp = is_ddp 
+        self.gpu_id = gpu_id
+        self.base_lr = base_lr
+        self.num_epochs = num_epochs
+        self.start_epoch = start_epoch
+        self.save_encoder = save_encoder
         
         # setup model and ema model
         self.encoder = encoder.to(gpu_id)
         self.decoder = decoder.to(gpu_id)
-        # self.decoder: DDP = DDP(
-        #     self.decoder,
-        #     device_ids=[gpu_id]
-        # )
-        
-        self.ema_decoder = AveragedModel(
-                model=self.decoder, #self.decoder.module, 
-                multi_avg_fn=get_ema_multi_avg_fn(0.999)
-            )
-        self.save_encoder = save_encoder
-        if save_encoder:
-            # self.encoder: DDP = DDP(
-            #     self.encoder,
-            #     device_ids=[gpu_id]
-            # )
-            self.ema_encoder = AveragedModel(
-                model=self.encoder, #self.encoder.module, 
-                multi_avg_fn=get_ema_multi_avg_fn(0.999)
-            )
+        self._setup_encoder_decoder()
         
         # setup criterion, optimizer and lr schedulers
-        self.criterion = criterion
-        self.optimizer = optimizer
-
-        if lr_scheduler_cosine is None:
-            self.lr_scheduler_cosine = CosineAnnealingLR(
-                self.optimizer, 
-                T_max=num_epochs,
-                eta_min=5e-4
-            )
-        else:
-            self.lr_scheduler_cosine = lr_scheduler_cosine
+        self._setup_criterion_optim_scheduler(criterion, optimizer, lr_scheduler_cosine)
         
-        # setup dataloaders
+        # setup dataloader(s)
         self.train_loader = train_loader
         
-        # setup training configs
-        self.task = task
-        self.num_epochs = num_epochs
-        self.gpu_id = gpu_id
-        self.start_epoch = start_epoch
+        # setup paths to save model
         self.out_path = os.path.join(out_dir, formatted_today)
         self.decoder_out_path = os.path.join(out_dir, formatted_today, "decoder", )
         os.makedirs(self.decoder_out_path, exist_ok=True)
@@ -101,10 +79,76 @@ class AETrainer:
             os.makedirs(self.encoder_out_path, exist_ok=True)
         
 
+    def _setup_encoder_decoder(self):
+        if self.is_ddp:
+            self.encoder: DDP = DDP(
+                self.encoder,
+                device_ids=[self.gpu_id]
+            )
+            self.decoder: DDP = DDP(
+                self.decoder,
+                device_ids=[self.gpu_id]
+            )
+
+            self.ema_encoder = AveragedModel(
+                model=self.encoder.module, 
+                multi_avg_fn=get_ema_multi_avg_fn(AVG_WEIGHT)
+            )
+            self.ema_decoder = AveragedModel(
+                    model=self.decoder.module, 
+                    multi_avg_fn=get_ema_multi_avg_fn(AVG_WEIGHT)
+                )
+            
+        else:
+            self.ema_encoder = AveragedModel(
+                model=self.encoder, 
+                multi_avg_fn=get_ema_multi_avg_fn(AVG_WEIGHT)
+            )
+            self.ema_decoder = AveragedModel(
+                    model=self.decoder, 
+                    multi_avg_fn=get_ema_multi_avg_fn(AVG_WEIGHT)
+                )
+    
+    
+    def _setup_criterion_optim_scheduler(
+        self,
+        criterion,
+        optimizer,
+        lr_scheduler_cosine
+    ):
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+        if lr_scheduler_cosine is None:
+            self.lr_scheduler_cosine = CosineAnnealingLR(
+                self.optimizer, 
+                T_max=self.num_epochs,
+                eta_min=self.base_lr/2
+            )
+        else:
+            self.lr_scheduler_cosine = lr_scheduler_cosine
+    
+    
+    def _save_modules(
+        self, 
+        epoch,
+        save_path: str, 
+        *state_dicts
+    ):
+        model_state_dict, optim_state_dict, scheduler_state_dict = state_dicts
+        torch.save({
+            "num_epoch": epoch,
+            "model_state_dict": model_state_dict,
+            "optimizer_state_dict": optim_state_dict,
+            "lr_cosine_state_dict": scheduler_state_dict,
+        }, save_path)
+    
+    
     def fit(self):
         r"""
         Perform fitting loop and validation
         """
+        # For logging
         if self.gpu_id == 0:
             train_csv_path = os.path.join(self.out_path, "train_metrics.csv")
             with open(train_csv_path, mode='w+', newline='') as train_csvfile:
@@ -127,23 +171,20 @@ class AETrainer:
         # save EMA model
         if self.gpu_id == 0:
             save_path = os.path.join(self.decoder_out_path, f"decoder_ema_model.pth")
-            torch.save({
-                    "num_epoch": self.num_epochs,
-                    "model_state_dict": self.ema_decoder.module.state_dict(),
-                    "optimizer_state_dict": self.optimizer.state_dict(),
-                    "lr_cosine_state_dict": self.lr_scheduler_cosine.state_dict(),
-                }, save_path)
+            decoder_state_dicts = (
+                self.ema_decoder.module.state_dict(), 
+                self.optimizer.state_dict(), 
+                self.lr_scheduler_cosine.state_dict()
+            )
+            self._save_modules(self.num_epochs, save_path, *decoder_state_dicts)
             if self.save_encoder:
                 save_path = os.path.join(self.encoder_out_path, f"encoder_ema_model.pth")
-                torch.save({
-                    "num_epoch": self.num_epochs,
-                    "model_state_dict": self.ema_encoder.module.state_dict(),
-                    "optimizer_state_dict": self.optimizer.state_dict(),
-                    "lr_cosine_state_dict": self.lr_scheduler_cosine.state_dict(),
-                }, save_path)
-        
-        # train_csvfile.close()
-        # val_csvfile.close()
+                encoder_state_dicts = (
+                    self.ema_encoder.module.state_dict(), 
+                    self.optimizer.state_dict(), 
+                    self.lr_scheduler_cosine.state_dict()
+                )
+                self._save_modules(self.num_epochs, save_path, *encoder_state_dicts)
      
      
     def _run_one_step(self, data) :
@@ -199,54 +240,37 @@ class AETrainer:
                     
                     # save this step for backup...
                     save_path = os.path.join(self.decoder_out_path, "decoder_last.pth")
-                    torch.save({
-                        "epoch": epoch,
-                        "model_state_dict": self.decoder.state_dict(), # self.decoder.module.state_dict(),
-                        "optimizer_state_dict": self.optimizer.state_dict(),
-                        "lr_cosine_state_dict": self.lr_scheduler_cosine.state_dict(),
-                    }, save_path)
+                    state_dicts = (
+                        self.optimizer.state_dict(), 
+                        self.lr_scheduler_cosine.state_dict()
+                    )
+                    if self.is_ddp:
+                        decoder_state_dicts = (self.decoder.module.state_dict(),) + state_dicts
+                    else:
+                        decoder_state_dicts = (self.decoder.state_dict(),) + state_dicts
+                    self._save_modules(epoch, save_path, *decoder_state_dicts)
 
                     if self.save_encoder:
                         save_path = os.path.join(self.encoder_out_path, "encoder_last.pth")
-                        torch.save({
-                            "epoch": epoch,
-                            "model_state_dict": self.encoder.state_dict(), # self.encoder.module.state_dict(),
-                            "optimizer_state_dict": self.optimizer.state_dict(),
-                            "lr_cosine_state_dict": self.lr_scheduler_cosine.state_dict(),
-                        }, save_path)                    
+                        if self.is_ddp:
+                            encoder_state_dicts = (self.encoder.module.state_dict(),) + state_dicts
+                        else:
+                            encoder_state_dicts = (self.encoder.state_dict(),) + state_dicts
+                        self._save_modules(epoch, save_path, *encoder_state_dicts)             
                 
                 # clear cache
-                # torch.cuda.empty_cache()
-                # gc.collect()
+                torch.cuda.empty_cache()
                 # break
 
         # calculate averaged loss
         loss = ema_loss
         print(f'Epoch {epoch+1} Loss: {loss:4f}')
         print()
-        
-        # also save model state dict along with optimizer's and scheduler's at the end of every epoch
-        # os.makedirs(os.path.join(self.out_path, "epochs"), exist_ok=True)
-        # save_path = os.path.join(self.out_path, "epochs", f"epoch_{epoch+1}.pth")
- 
-        # # os.makedirs(save_path, exist_ok=True)
-        # torch.save({
-        #     "epoch": epoch,
-        #     "model_state_dict": self.model.state_dict(),
-        #     "optimizer_state_dict": self.optimizer.state_dict(),
-        #     "lr_increase_state_dict": self.lr_scheduler_increase.state_dict(),
-        #     "lr_cosine_state_dict": self.lr_scheduler_cosine.state_dict(),
-        #     "loss": loss,
-        #     "metrics": metrics
-        #     }, save_path)
- 
-        # # reset metrics tracker after every training epoch
-        # self.metrics.reset()
 
         return [epoch + 1, f"{loss:4f}"]
 
 
-    @torch.inference_mode()
+    """ @torch.inference_mode()
     def val(self, epoch):
         self.criterion.running_loss = 0.
         self.model.eval()
@@ -327,4 +351,4 @@ class AETrainer:
         # reset metrics tracker after every validating epoch
         self.metrics.reset()
         
-        return [epoch + 1, f"{loss:4f}", f"{acc:4f}", f"{IoU:4f}", f"{mIoU:4f}"]
+        return [epoch + 1, f"{loss:4f}", f"{acc:4f}", f"{IoU:4f}", f"{mIoU:4f}"]"""
--- train_vae.py
+++ train_vae.py
@@ -20,14 +20,81 @@ from optimizer.optimizer import OPTIMIZERS
 from augmentation.augmentation import CustomAug
 
 
-def set_seed(seed):
+NUM_DEVICE = torch.cuda.device_count()
+
+
+def get_args() -> None:
+    """
+    Getting arguments parser method
+
+    Returns:
+        None
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Training args')
+
+    # task
+    parser.add_argument(
+        '--task', type=str, default="mnist", required=False, 
+        help='Dataset to train model on, valid values are one of [flowers102, mnist, fashion_mnist]'
+    )
+    
+    # DDP (Distributed Data Parallel) option
+    parser.add_argument(
+        '--is_dpp', action="store_true", 
+        help='Option for choosing training in DDP or normal training criteria'
+    )
+    
+    # image size
+    parser.add_argument('--img_size', type=int, default=32, help='Image size')
+    
+    # standard deviation of Decoder
+    parser.add_argument(
+        '--std_dec', type=float, default=1, 
+        help='Standard deviation of decoder'
+    )
+    
+    # Number of training epochs
+    parser.add_argument('--epochs', type=int, default=10, help='Num epochs')
+    
+    # Batch size
+    parser.add_argument('--batch', type=int, default=32, help='Batch size')
+    
+    # Random seed
+    parser.add_argument('--seed', type=int, default=42, help='Random seed for training')
+    
+    # Checkpoint of encoder to resume
+    parser.add_argument(
+        '--ckpt_encoder', nargs='?', default = None,
+        help='Checkpoint of pretrained encoder for coutinue training'
+    )
+    
+    # Checkpoint of decoder to resume
+    parser.add_argument(
+        '--ckpt_decoder', nargs='?', default = None,
+        help='Checkpoint of pretrained decoder for coutinue training'
+    )
+
+    args = parser.parse_args()
+    
+    return args
+
+
+def set_seed(seed: int) -> None:
+    """
+    Setting random seed method
+
+    Args:
+        seed (int): Seed
+    """
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)  
     np.random.seed(seed)
     random.seed(seed)
     
 
-def ddp_setup(rank: int, world_size: int):
+def set_ddp(rank: int, world_size: int) -> None:
     """
     Init DDP
 
@@ -45,138 +112,12 @@ def ddp_setup(rank: int, world_size: int):
     )
     if rank == 0:
         time.sleep(30)
-
-
-NUM_DEVICE = torch.cuda.device_count()
-
-
-def main(
-    rank: int,
-    # world_size: int,
-    task: str,
-    img_size: int | tuple,
-    std_dec: float,
-    num_epochs: int,
-    batch_size: int,
-    ckpt_encoder,
-    ckpt_decoder
-):
-    # ddp_setup(rank, world_size)
-    
-    if task == "flowers102":
-        from datasets.flowers102 import Flowers102AE
-        from experiments_setup.flowers102.backbone_config import get_ae_configs
-        from experiments_setup.flowers102.experiment_config import (
-            OPTIMIZER_NAME,
-            OPTIM_ARGS
-        )
-        TRAIN_DS = Flowers102AE(transform=CustomAug(img_size), split="train")
-        VAL_DS = Flowers102AE(transform=CustomAug(img_size), split="val")
-        TEST_DS = Flowers102AE(transform=CustomAug(img_size), split="test")
-        TRAIN_DS = torch.utils.data.ConcatDataset([TRAIN_DS, VAL_DS, TEST_DS])
-
-    elif task == "fashion_mnist":
-        from datasets.fashion_mnist import load_transformed_fashionMNIST
-        from experiments_setup.mnist.backbone_config import get_ae_configs
-        from experiments_setup.mnist.experiment_config import (
-            OPTIMIZER_NAME,
-            OPTIM_ARGS
-        )
-        TRAIN_DS = load_transformed_fashionMNIST(img_size)
         
-    elif task == "mnist":
-        from datasets.mnist import load_transformed_MNIST
-        from experiments_setup.mnist.backbone_config import get_ae_configs
-        from experiments_setup.mnist.experiment_config import (
-            OPTIMIZER_NAME,
-            OPTIM_ARGS
-        )
-        TRAIN_DS = load_transformed_MNIST(img_size)
-    
-    AE_CONFIGS = get_ae_configs()        
-    IS_PIN_MEMORY = True
-    NUM_WORKERS = 4
-    
-    out_dir = os.path.join("./weights/VAEweights", task)
 
-    # these hyperparams depend on the dataset / experiment
-    otim = OPTIMIZER_NAME
-    optim_args = OPTIM_ARGS
-
-    # initialize the encoder, decoder and optimizer
-    latent_shape = _pair(img_size//8)
-    decoder = VAEDecoder(
-        std_dec=std_dec, 
-        **AE_CONFIGS["decoder"], 
-        device=rank
-    )
-    encoder = VAEEncoder(
-        latent_shape=latent_shape,
-        **AE_CONFIGS["encoder"], 
-        device=rank
-    )
-    optimizer = OPTIMIZERS[otim](
-        [
-            {"params": encoder.parameters()},
-            {"params": decoder.parameters()},
-        ],
-        **optim_args,
-    )
-    
-    # print(optimizer)
-
-    # load check point...
-    if ckpt_encoder:
-        ckpt_encoder = torch.load(ckpt_encoder, weights_only=False)
-        encoder.load_state_dict(ckpt_encoder["model_state_dict"])        
-
-    if ckpt_decoder:
-        ckpt_decoder = torch.load(ckpt_decoder, weights_only=False)
-        decoder.load_state_dict(ckpt_decoder["model_state_dict"])
-        
-    last_epoch = 0
-    lr_scheduler_cosine = None
-    
-    # Compile modules
-    encoder.compile(fullgraph=True, backend="cudagraphs")
-    decoder.compile(fullgraph=True, backend="cudagraphs")   
-
-    # setup dataloaders
-    train_loader = DataLoader(
-        TRAIN_DS, 
-        batch_size=batch_size, 
-        shuffle=True, 
-        # sampler=DistributedSampler(TRAIN_DS),
-        num_workers=NUM_WORKERS,
-        drop_last=True,
-        pin_memory=IS_PIN_MEMORY
-    )
-
-    # call the trainer
-    from losses.ELBO import ELBOLoss
-    criterion = ELBOLoss(M=16)
-    trainer = AETrainer(
-        encoder=encoder,
-        decoder=decoder,
-        task=task,
-        criterion=criterion,
-        optimizer=optimizer,
-        num_epochs=num_epochs,
-        start_epoch=last_epoch,
-        save_encoder=True,
-        train_loader=train_loader,
-        out_dir=out_dir,
-        lr_scheduler_cosine=lr_scheduler_cosine,
-        gpu_id=rank
-    )
-    trainer.fit()
-    
-    # destroy_process_group()
-
-
-if __name__ == "__main__":
-
-    # environment setup
+def set_env() -> None:
+    """
+    Method for setting other enviroment variables
+    """
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     # os.environ["TORCH_LOGS"] = "+dynamo"
@@ -197,28 +138,190 @@ if __name__ == "__main__":
         torch.multiprocessing.set_start_method('spawn')
     except RuntimeError:
         pass
+
+
+def main(
+    rank: int,
+    world_size: int,
+    is_ddp: bool,
+    task: str,
+    img_size: int | tuple,
+    std_dec: float,
+    num_epochs: int,
+    batch_size: int,
+    ckpt_encoder,
+    ckpt_decoder
+):
+    """
+    Main function for training
+
+    Args:
+        rank (int): GPU id. In the case of DDP, this will be loaded automatically by the DPP setup.
+            In the normal training criteria, GPU id should be loaded by the default value 0.
+        world_size (int): Number of device.
+        is_ddp (bool): Whether the training setting is DDP or normal criteria.
+        task (str): Task to train the model on.
+        img_size (int | tuple): Image size.
+        std_dec (float): Standard deviation of the decoder distribution.
+        num_epochs (int): Number of training epoch.
+        batch_size (int): Batch size.
+        ckpt_encoder (_type_): Checkpoint of encoder to continue the training process.
+        ckpt_decoder (_type_): Checkpoint of decoder to continue the training process.
+    """
+    def _get_configs(
+        task: str,
+        img_size: int | tuple,
+    ):
+        """
+        Return dataset and configuration elements of model
+        """
+        if task == "flowers102":
+            from datasets.flowers102 import Flowers102AE
+            from experiments_setup.flowers102.backbone_config import get_ae_configs
+            from experiments_setup.flowers102.experiment_config import (
+                OPTIMIZER_NAME,
+                OPTIM_ARGS
+            )
+            TRAIN_DS = Flowers102AE(transform=CustomAug(img_size), split="train")
+            VAL_DS = Flowers102AE(transform=CustomAug(img_size), split="val")
+            TEST_DS = Flowers102AE(transform=CustomAug(img_size), split="test")
+            TRAIN_DS = torch.utils.data.ConcatDataset([TRAIN_DS, VAL_DS, TEST_DS])
+
+        elif task == "fashion_mnist":
+            from datasets.fashion_mnist import load_transformed_fashionMNIST
+            from experiments_setup.mnist.backbone_config import get_ae_configs
+            from experiments_setup.mnist.experiment_config import (
+                OPTIMIZER_NAME,
+                OPTIM_ARGS
+            )
+            TRAIN_DS = load_transformed_fashionMNIST(img_size)
+            
+        elif task == "mnist":
+            from datasets.mnist import load_transformed_MNIST
+            from experiments_setup.mnist.backbone_config import get_ae_configs
+            from experiments_setup.mnist.experiment_config import (
+                OPTIMIZER_NAME,
+                OPTIM_ARGS
+            )
+            TRAIN_DS = load_transformed_MNIST(img_size)
+            
+        return TRAIN_DS, (get_ae_configs, OPTIMIZER_NAME, OPTIM_ARGS)
+    
+    def _get_modules(
+        img_size,
+        ckpt_encoder,
+        ckpt_decoder,
+        backend: str = "cudagraphs"
+    ):
+        latent_shape = _pair(img_size//8)
+        decoder = VAEDecoder(
+            std_dec=std_dec, 
+            **AE_CONFIGS["decoder"], 
+            device=rank
+        )
+        encoder = VAEEncoder(
+            latent_shape=latent_shape,
+            **AE_CONFIGS["encoder"], 
+            device=rank
+        )
+        optimizer = OPTIMIZERS[OPTIMIZER_NAME](
+            [
+                {"params": encoder.parameters()},
+                {"params": decoder.parameters()},
+            ],
+            **OPTIM_ARGS,
+        )
+
+        # load check point...
+        if ckpt_encoder:
+            ckpt_encoder = torch.load(ckpt_encoder, weights_only=False)
+            encoder.load_state_dict(ckpt_encoder["model_state_dict"])        
+
+        if ckpt_decoder:
+            ckpt_decoder = torch.load(ckpt_decoder, weights_only=False)
+            decoder.load_state_dict(ckpt_decoder["model_state_dict"])
+
+        # Compile modules
+        encoder.compile(fullgraph=True, backend=backend)
+        decoder.compile(fullgraph=True, backend=backend)
+        return encoder, decoder, optimizer
+        
+    
+    if is_ddp:
+        set_ddp(rank, world_size)
+    
+    # these hyperparams depend on the dataset / experimen
+    TRAIN_DS, (get_ae_configs, OPTIMIZER_NAME, OPTIM_ARGS) = _get_configs(task, img_size)
+    AE_CONFIGS = get_ae_configs()        
+    IS_PIN_MEMORY = True
+    NUM_WORKERS = 4
+    
+    out_dir = os.path.join("./weights/VAEweights", task)
+
+    # initialize the encoder, decoder and optimizer
+    encoder, decoder, optimizer = _get_modules(
+        img_size, 
+        ckpt_encoder,
+        ckpt_decoder
+    )
+    
+    # setup dataloaders
+    if is_ddp:
+        train_loader = DataLoader(
+            TRAIN_DS, 
+            batch_size=batch_size, 
+            shuffle=False, 
+            sampler=DistributedSampler(TRAIN_DS),
+            num_workers=NUM_WORKERS,
+            drop_last=True,
+            pin_memory=IS_PIN_MEMORY
+        )
+    else:
+        train_loader = DataLoader(
+            TRAIN_DS, 
+            batch_size=batch_size, 
+            shuffle=True, 
+            num_workers=NUM_WORKERS,
+            drop_last=True,
+            pin_memory=IS_PIN_MEMORY
+        )
+
+    # setup the trainer
+    from losses.ELBO import ELBOLoss
+    
+    last_epoch = 0
+    lr_scheduler_cosine = None
+    criterion = ELBOLoss(M=16)
+    trainer = AETrainer(
+        encoder=encoder,
+        decoder=decoder,
+        task=task,
+        criterion=criterion,
+        optimizer=optimizer,
+        num_epochs=num_epochs,
+        start_epoch=last_epoch,
+        save_encoder=True,
+        train_loader=train_loader,
+        out_dir=out_dir,
+        lr_scheduler_cosine=lr_scheduler_cosine,
+        gpu_id=rank
+    )
+    trainer.fit()
+    
+    if is_ddp:
+        destroy_process_group()
+
+
+if __name__ == "__main__":
+    # environment setup
+    set_env()
     
     # arguments parser
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Training args')
-
-    parser.add_argument('--task', type=str, default="mnist", required=False, help='Dataset to train model on, valid values are one of [flowers102, mnist, fashion_mnist]')
-    parser.add_argument('--img_size', type=int, default=32, help='Image size')
-    # parser.add_argument('--scale', type=float, default=0.25, required=False, help='Model scale')
-    parser.add_argument('--std_dec', type=float, default=1.25, help='Standard deviation of decoder')
-    parser.add_argument('--epochs', type=int, default=10, help='Num epochs')
-    parser.add_argument('--batch', type=int, default=32, help='batch size')
-    parser.add_argument('--seed', type=int, default=42, help='seed for training')
-    parser.add_argument('--ckpt_encoder', nargs='?', default = None,
-                        help='Checkpoint of pretrained encoder for coutinue training')
-    parser.add_argument('--ckpt_decoder', nargs='?', default = None,
-                        help='Checkpoint of pretrained decoder for coutinue training')
-
-    args = parser.parse_args()
+    args = get_args()
     
     # setup model hyperparameters and training parameters
     task = args.task
+    is_ddp = args.is_ddp
     img_size = args.img_size
     std_dec = args.std_dec
     num_epochs = args.epochs
@@ -230,8 +333,8 @@ if __name__ == "__main__":
     
     world_size = NUM_DEVICE
     args = (
-        0, # device
-        # world_size, 
+        world_size, 
+        is_ddp,
         task, 
         img_size, 
         std_dec,
@@ -240,5 +343,8 @@ if __name__ == "__main__":
         ckpt_encoder,
         ckpt_decoder
     )
-    # mp.spawn(main, args=args, nprocs=world_size)
-    main(*args)
+
+    if is_ddp:
+        mp.spawn(main, args=args, nprocs=world_size)
+    else:
+        main(rank=0, *args)
