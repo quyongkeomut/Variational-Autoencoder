2791e8fe02e0e07c18dfe6c595b51578a0412fee
Phuoc-Thinh, N <thinh.nguyen.dlai@gmail.com>
2025-06-17 18:52:24 +0700

    update image generating utils

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -16,27 +16,43 @@ REVERSE_TRANSFORMS = transforms.Compose([
 ])
 
 
-def save_animation(
-    xs, 
-    gif_name, 
+def show_animation(
+    xs: List[Tensor], 
     interval=300, 
     repeat_delay=5000
 ):
     fig = plt.figure()
     plt.axis('off')
     imgs = []
-
     for x_t in xs:
         im = plt.imshow(x_t, animated=True)
         imgs.append([im])
+    ani = animation.ArtistAnimation(fig, imgs, interval=interval, repeat_delay=repeat_delay, blit=True)
+    plt.close(fig)  # Prevent static image from showing
+    return HTML(ani.to_jshtml())  # Or use ani.to_html5_video()
 
+
+def save_animation(
+    xs, 
+    save_path, 
+    interval=300, 
+    repeat_delay=5000
+):
+    fig = plt.figure()
+    plt.axis('off')
+    imgs = []
+    for x_t in xs:
+        im = plt.imshow(x_t, animated=True)
+        imgs.append([im])
     animate = animation.ArtistAnimation(fig, imgs, interval=interval, repeat_delay=repeat_delay)
-    animate.save(gif_name)
+    animate.save(save_path)
+
 
 def show_tensor_image(
     image: Tensor,
 ):
     plt.imshow(image)
+    
 
 def to_image(tensor, to_pil=True):
     tensor = (tensor + 1) / 2
@@ -55,26 +71,8 @@ def plot_generated_images(
     plt.figure(figsize=_pair(figsize))
     nrows = 1
     ncols = len(results)
-    for i, img in enumerate(results.items()):
+    for i, img in enumerate(results):
         ax = plt.subplot(nrows, ncols, i+1)
         ax.set_axis_off()
         show_tensor_image(img)
     plt.show()
-    
-    
-def show_animation(
-    xs: List[Tensor], 
-    interval=300, 
-    repeat_delay=5000
-):
-    fig = plt.figure()
-    plt.axis('off')
-    imgs = []
-    for x_t in xs:
-        x_t = x_t.squeeze(0)
-        x_t = REVERSE_TRANSFORMS(x_t.detach().cpu())
-        im = plt.imshow(x_t, animated=True)
-        imgs.append([im])
-    ani = animation.ArtistAnimation(fig, imgs, interval=interval, repeat_delay=repeat_delay, blit=True)
-    plt.close(fig)  # Prevent static image from showing
-    return HTML(ani.to_jshtml())  # Or use ani.to_html5_video()
